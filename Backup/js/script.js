546a53794ddc16709536d9e290083c1cef142f8b
Jibin-Varghese-git <jibinvarghese@techversantinfotech.com>
2024-10-23 20:43:02 +0530

    23/10/24

---
+++ Backup/js/script.js
@@ -0,0 +1,101 @@
+var data = [{
+    start: 0,
+    duration: 15,
+    title: "Exercise"
+}, {
+    start: 25,
+    duration: 30,
+    title: "Travel to work"
+}, {
+    start: 30,
+    duration: 30,
+    title: "Plan day"
+}, {
+    start: 60,
+    duration: 15,
+    title: "Review yesterday's commits"
+}, {
+    start: 100,
+    duration: 15,
+    title: "Code review"
+}, {
+    start: 180,
+    duration: 90,
+    title: "Have lunch with John"
+}, {
+    start: 360,
+    duration: 30,
+    title: "Skype call"
+}, {
+    start: 370,
+    duration: 45,
+    title: "Follow up with designer"
+}, {
+    start: 400,
+    duration: 30,
+    title: "Push up branch"
+}
+];
+//INPUTS
+
+console.log(data[0])
+let start;
+let duration=0;
+let overlapParent=[];
+let overlapChild=[];
+let num=[];
+let div=document.createElement("div");
+let flag=1;
+
+    for( let i=0;i<data.length-1;i++)
+    {
+        
+       
+            let diff=data[i+1].start-data[i].start;
+            if(data[i].duration>diff)
+            {
+                overlapParent.push(i);
+                overlapChild.push(i+1);
+            }
+            else{
+                num.push(i);
+            }
+    }
+
+    for( let i=0;i<data.length;i++)
+    {
+            let div=document.createElement("div");
+
+            if(overlapChild.includes(i) && overlapChild.includes(i-1))
+            {
+                div.style.left='52%';
+                div.style.width='45%';
+            }
+            else if(overlapChild.includes(i) && overlapParent.includes(i))
+            {
+                div.style.width='45%';
+            }
+            else if(overlapParent.includes(i) )
+            {
+                div.style.left='52%';
+                div.style.width='45%';
+            }
+            else if(overlapChild.includes(i) )
+            {
+                div.style.width='45%';
+            }
+            
+            duration=data[i].duration + "px";
+            start=data[i].start + "px";
+
+            div.style.height=duration ;
+            div.style.top=start ;
+            div.innerHTML=data[i].title;
+            div.classList.add("time-child-div")
+            document.getElementById("time-inner-div").appendChild(div);
+
+            
+    }
+    
+   
+   
